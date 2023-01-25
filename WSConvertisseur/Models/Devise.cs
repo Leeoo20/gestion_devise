7b07cf292ef0fe26bcffa56017f755182e396109
Léo vernier <Leo.Vernier@etu.univ-smb.fr>
2023-01-25 08:53:15 +0100

    Ajout du modèle

---
+++ WSConvertisseur/Models/Devise.cs
@@ -0,0 +1,52 @@
+﻿namespace WSConvertisseur.Models
+{
+    public class Devise
+    {
+        private int id;
+        private string? nomDevise;
+        private double taux;
+        public Devise()
+        {
+
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string? NomDevise
+        {
+            get
+            {
+                return nomDevise;
+            }
+
+            set
+            {
+                nomDevise = value;
+            }
+        }
+
+        public double Taux
+        {
+            get
+            {
+                return taux;
+            }
+
+            set
+            {
+                taux = value;
+            }
+        }
+    }
+}
